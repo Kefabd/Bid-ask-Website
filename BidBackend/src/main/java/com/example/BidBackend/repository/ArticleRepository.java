284e8b079f7be37ac1f30466f7a037d652204060
Hanine2002 <hanine.nadia@ensam-casa.com>
2024-01-11 17:22:55 +0100

    'reposi'

--- BidBackend/src/main/java/com/example/BidBackend/repository/ArticleRepository.java
+++ BidBackend/src/main/java/com/example/BidBackend/repository/ArticleRepository.java
@@ -1,4 +1,17 @@
 package com.example.BidBackend.repository;
 
-public interface ArticleRepository {
+import com.example.BidBackend.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ArticleRepository extends JpaRepository<Article,Long> {
+    @Query("SELECT a FROM Article a WHERE a.description = :description")
+    List<Article> findByDescription(@Param("description") Long description);
+
+    Optional<Article> findById(Long id);
+
 }
